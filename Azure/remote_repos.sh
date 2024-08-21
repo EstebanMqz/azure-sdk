2e4412a48c3efa2511f1200c89e75b55988700e0
EstebanMqz <emarquez1895@gmail.com>
2024-08-21 14:22:34 -0600

    README.md &&.gitignore

--- Azure/remote_repos.sh
+++ Azure/remote_repos.sh
@@ -1,14 +1,15 @@
 # mAKE a Script that will clone the remote repository given as var and then have it push to the remote repository and check with git remote -v if the pushes were sueccessful.
-
 #!/bin/bash
 
-# Clone the remote repository
-https://github.com/EstebanMqz/azure-sdk.git=$1 && git clone $1 && cd $(basename $1.git)
-# Change dir to repo-
+#> Clone remote URL.
+https://github.com/EstebanMqz/azure-sdk.com=$1 && git clone $1 && cd $(basename $1.git)
+
+
+# Change dir. to repo.
 cd {$pwd} || touch README.md && touch .gitignore
+# Add, Commit & Push.
+git add -A && git commit -m "README.md &&.gitignore" && git push --mirror
+#Publish the repo in my profile with the remote URL. 
+git remote -v && cd ..mv remote_repos.sh subdir/.sh && ./remote_repos.sh
+git mv remote_repos.sh .sh #> End of Script.
 
-# Add the README.md file to the repository
-git add -A && git commit -m "README.md & .gitignore" && git push --mirror
-
-REM Extract the pwd of the repo in the remote repository
-remote_pwd=$(git remote -v | grep fetch | awk '{pr
